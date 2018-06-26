187bcb6629a6a42aaa6baa342dd0814deddd69a4
javilaurin <javilaurin@gmail.com>
2018-06-26 19:51:43 +0200

    Before region images

--- app/src/main/java/com/laurinware/pokebrain/View/RegionListActivity.java
+++ app/src/main/java/com/laurinware/pokebrain/View/RegionListActivity.java
@@ -1,10 +1,8 @@
 package com.laurinware.pokebrain.View;
 
-import android.app.AlertDialog;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -16,12 +14,9 @@ import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.laurinware.pokebrain.Model.PokemonList;
 import com.laurinware.pokebrain.Model.RegionItem;
@@ -87,44 +82,6 @@ public class RegionListActivity extends AppCompatActivity {
                 recyclerView.setAdapter(new RegionListActivity.SimpleItemRecyclerViewAdapter(act, regionNamesList.getResults(), mTwoPane));
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id){
-            case R.id.action_settings:
-                // TODO
-                Toast.makeText(this, "Settings activity", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_about:
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                builder.setMessage(R.string.action_about_text)
-                        .setCancelable(false)
-                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                //do things
-                            }
-                        });
-                AlertDialog alert = builder.create();
-                alert.show();
-                return true;
-        }
-
-
-        return super.onOptionsItemSelected(item);
     }
 
     public static class SimpleItemRecyclerViewAdapter
